74ad4b650af33512a037b85354b1db92dab26d69
suchithms19 <mssuchith2@gmail.com>
2025-11-05 12:25:56 +0530

    chore: move all models

---
+++ server/src/models/index.js
@@ -0,0 +1,15 @@
+const User = require('./user.schema');
+const Question = require('./question.schema');
+const Submission = require('./submission.schema');
+const Badge = require('./badge.schema');
+const Notification = require('./notification.schema');
+const Analytics = require('./analytics.schema');
+
+module.exports = {
+    User,
+    Question,
+    Submission,
+    Badge,
+    Notification,
+    Analytics
+};
