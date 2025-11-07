5ed83de6f384a47c3d157005ba2c79240720b189
Sanjana2707 <sanjanagowda2707@gmail.com>
2025-11-07 15:44:39 +0530

    fix: reorder dotenv configuration and PORT assignment for consistency

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT;
 const adminQuestionsRouter = require('./src/routes/admin/question.routes');
 const adminUsersRouter = require('./src/routes/admin/user.routes');
-const PORT = process.env.PORT || 3000;
 
-dotenv.config();
 const app = express();
-
 app.use(express.json());
-app.use('/admin/questions',adminQuestionsRouter);
-app.use('/admin/users',adminUsersRouter);
 
 app.get("/ping", (_req, res) => {
   res.send("pong");
 });
+
+app.use('/admin/questions',adminQuestionsRouter);
+app.use('/admin/users',adminUsersRouter);
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
