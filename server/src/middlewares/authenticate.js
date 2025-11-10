34737c0d8080f3b7e37df80e135e15e97b6d9b68
suchithms19 <mssuchith2@gmail.com>
2025-11-10 21:14:51 +0530

    refactor: remove unused middleware and consolidate admin access control into authenticate.js

--- server/src/middlewares/authenticate.js
+++ server/src/middlewares/authenticate.js
@@ -28,4 +28,16 @@ const authenticate = (req, res, next) => {
   }
 };
 
-module.exports = { authenticate };
+const isAdmin = (req, res, next) => {
+  if (!req.user || req.user.role !== 'ADMIN') {
+    return res.status(403).json({
+      success: false,
+      error: {
+        message: 'Admin access required',
+      },
+    });
+  }
+  next();
+};
+
+module.exports = { authenticate, isAdmin };
